0383129dd791a24e7dbf17694225bca47287b210
daothihuyen64 <daothihuyennv64@gmail.com>
2024-09-27 00:34:37 +0700

    add folder for backend

---
+++ web-movie/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Chào mừng đến với ứng dụng Vue.js của tôi!</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+/* CSS cho App */
+</style>
---
+++ web-movie/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Xin chào, thế giới!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+/* CSS cho component này */
+</style>
