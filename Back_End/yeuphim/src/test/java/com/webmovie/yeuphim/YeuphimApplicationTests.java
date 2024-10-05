f7b9a7c292ebad1bf5abbc64e89c44fd293bcb90
HuyenDang <huyendtb22dccn400@gmail.com>
2024-10-05 20:01:10 +0700

    cả project

---
+++ Back_End/yeuphim/src/test/java/com/webmovie/yeuphim/YeuphimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmovie.yeuphim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeuphimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
