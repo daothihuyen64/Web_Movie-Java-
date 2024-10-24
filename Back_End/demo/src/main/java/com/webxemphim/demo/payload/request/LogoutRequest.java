ae1f0575969f3ffe4aa5e48d38ac3cbf521795ff
HuyenDang <huyendtb22dccn400@gmail.com>
2024-10-24 20:07:14 +0700

    Đã thêm role vào phần trả về Đăng Nhập, Sửa tính năng Đăng xuất thành RequestBody

---
+++ Back_End/demo/src/main/java/com/webxemphim/demo/payload/request/LogoutRequest.java
@@ -0,0 +1,14 @@
+package com.webxemphim.demo.payload.request;
+
+public class LogoutRequest {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
